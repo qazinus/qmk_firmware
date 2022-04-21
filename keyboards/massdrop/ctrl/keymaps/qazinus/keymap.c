5125073c7105d1c20381fca4a4941fa56becf2e0
qazinus <qazinus@gmail.com>
2022-04-21 18:34:19 -0400

    add keymap.h

--- keyboards/massdrop/ctrl/keymaps/qazinus/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/qazinus/keymap.c
@@ -1,21 +1,10 @@
 #include QMK_KEYBOARD_H
-
-// #include "keymap.h"
+#include "keymap.h"
 
 // static uint16_t idle_timer;             // Idle LED timeout timer
 // static uint8_t idle_second_counter;     // Idle LED seconds counter, counts seconds not milliseconds
 // static uint8_t key_event_counter;       // This counter is used to check if any keys are being held
 
-
-enum ctrl_keycodes {
-    U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
-    U_T_AGCR,              //USB Toggle Automatic GCR control
-    DBG_TOG,               //DEBUG Toggle On / Off
-    DBG_MTRX,              //DEBUG Toggle Matrix Prints
-    DBG_KBD,               //DEBUG Toggle Keyboard Prints
-    DBG_MOU,               //DEBUG Toggle Mouse Prints
-    MD_BOOT,               //Restart into bootloader after hold timeout
-};
 
 // static const char * sendstring_commands[] = {
 //     "git init ",
@@ -45,7 +34,7 @@ enum ctrl_keycodes {
 // };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [_KL] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             KC_PSCR, KC_SLCK, KC_PAUS,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN,
@@ -53,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                              KC_UP,
         KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(1),   KC_APP,  KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT
     ),
-    [1] = LAYOUT(
+    [_FL] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            KC_MUTE, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   KC_MPLY, KC_MSTP, KC_VOLU,
         _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, _______, _______, _______, _______,   KC_MPRV, KC_MNXT, KC_VOLD,
@@ -78,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // #define _______ {0, 0, 0}
 
 // const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
-//     [_FL] = {
+//     [_KL] = {
 //         _______, CORAL,   CORAL,   _______, _______, CORAL,   CORAL,   _______, _______, CORAL,   _______, YELLOW,  YELLOW,           TEAL,    GOLD,   GOLD,
 //         _______, _______, PINK,    PINK,    PINK,    PINK,    _______, _______, _______, GREEN,   GREEN,   GREEN,   GREEN,   _______, TEAL,    TEAL,   TEAL,
 //         ORANGE,  ORANGE,  ORANGE,  ORANGE,  ORANGE,  ORANGE,  _______, AZURE,   AZURE,   _______, _______, _______, _______, _______, TEAL,    TEAL,   TEAL,
@@ -86,37 +75,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //         _______, ORANGE,  _______, CORAL,   _______, AZURE,   AZURE,   _______, _______, _______, _______, AZURE,                              SPRING,
 //         _______, _______, _______,                   _______,                            _______, PINK,    _______, AZURE,            _______, SPRING, _______
 //     },
-//     [_ML] = {
-//         _______, GOLD,    GOLD,    GOLD,   _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
-//         _______, MAGENT,  MAGENT,  MAGENT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-//         _______, MAGENT,  GOLD,    MAGENT, GOLD,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-//         _______, GOLD,    GOLD,    GOLD,   GOLD,    _______, _______, _______, _______, _______, _______, _______, _______,
-//         _______, _______, _______, CORAL,  _______, _______, _______, _______, _______, _______, _______, _______,                            GOLD,
-//         _______, PINK,    _______,                  _______,                                     _______, PINK,    _______, _______, GOLD,    GOLD,    GOLD
-//     },
-//     [_GL] = {
-//         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
-//         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-//         _______, _______, _______, _______, BLUE,    AZURE,   AZURE,   _______, CYAN,    _______, TURQ,    PURPLE,  _______, _______, _______, _______, _______,
-//         _______, CORAL,   GREEN,   GREEN,   TURQ,    _______, _______, _______, _______, GREEN,   _______, _______, _______,
-//         _______, CYAN,    CHART,   TURQ,    ORANGE,  CHART,   _______, CHART,   _______, _______, _______, _______,                            _______,
-//         _______, _______, _______,                   _______,                            _______, PINK,    _______, _______,          _______, _______, _______
-//     },
-//     [_VL] = {
-//         PURPLE,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
-//         _______, _______, _______, TURQ,    _______, _______, _______, _______, TURQ,    _______, _______, _______, _______, _______, _______, _______, _______,
-//         _______, AZURE,   AZURE,   AZURE,   PURPLE,  _______, BLUE,    PURPLE,  PURPLE,  PURPLE,  BLUE,    _______, _______, _______, _______, _______, _______,
-//         _______, PURPLE,  _______, BLUE,    _______, GOLD,    GOLDEN,  GOLDEN,  GOLDEN,  GOLDEN,  _______, _______, _______,
-//         _______, _______, BLUE,    BLUE,    _______, _______, TURQ,    _______, _______, _______, TURQ,    _______,                            _______,
-//         _______, _______, _______,                   _______,                            _______, PINK,    _______, _______,          _______, _______, _______
-//     },
-//     [_YL] = {
-//         RED,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
-//         CHART,   BLUE,    BLUE,    BLUE,    BLUE,    BLUE,    BLUE,    BLUE,    BLUE,    BLUE,    BLUE,    _______, _______, _______, _______, PURPLE,  PURPLE,
-//         _______, RED,     _______, BLUE,    _______, GOLD,    _______, _______, GREEN,   _______, MAGENT,  _______, GOLD,    GOLD,    _______, PURPLE,  PURPLE,
-//         _______, BLUE,    _______, BLUE,    _______, MAGENT,  _______, GREEN,   GREEN,   GREEN,   MAGENT,  _______, _______,
-//         _______, ORANGE,  ORANGE,  _______, _______, _______, _______, RED,     MAGENT,  MAGENT,  _______, _______,                            GREEN,
-//         _______, _______, _______,                   _______,                   _______, PINK,    _______, _______,                   BLUE,    GREEN,   BLUE
+//     [_FL] = {
+//         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______, _______,
+//         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______,
+//         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______,
+//         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+//         _______, _______, _______, _______, _______, _______, AZURE  , _______, _______, _______, _______, _______,                              _______,
+//         _______, _______, _______,                   _______,                            _______, _______, _______, _______,            _______, _______, _______
 //     },
 // };
 
@@ -125,22 +90,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // #endif
 
 // // Runs just one time when the keyboard initializes.
-// void matrix_init_user(void) {
-//     // Enable or disable debugging
-//     debug_enable=true;
-//     debug_matrix=true;
-//     debug_keyboard=true;
-//     debug_mouse=true;
+void matrix_init_user(void) {
+    // // Enable or disable debugging
+    // debug_enable=true;
+    // debug_matrix=true;
+    // debug_keyboard=true;
+    // debug_mouse=true;
 
-//     idle_second_counter = 0;                            // Counter for number of seconds keyboard has been idle.
-//     key_event_counter = 0;                              // Counter to determine if keys are being held, neutral at 0.
-//     rgb_time_out_seconds = RGB_DEFAULT_TIME_OUT;        // RGB timeout initialized to its default configure in keymap.h
-//     rgb_time_out_enable = false;                        // Disable RGB timeout by default. Enable using toggle key.
-//     rgb_time_out_user_value = false;                    // Has to have the same initial value as rgb_time_out_enable.
-//     rgb_enabled_flag = true;                            // Initially, keyboard RGB is enabled. Change to false config.h initializes RGB disabled.
-//     rgb_time_out_fast_mode_enabled = false;             // RGB timeout fast mode disabled initially.
-//     rgb_time_out_saved_flag = rgb_matrix_get_flags();   // Save RGB matrix state for when keyboard comes back from ide.
-// };
+    // idle_second_counter = 0;                            // Counter for number of seconds keyboard has been idle.
+    // key_event_counter = 0;                              // Counter to determine if keys are being held, neutral at 0.
+    // rgb_time_out_seconds = RGB_DEFAULT_TIME_OUT;        // RGB timeout initialized to its default configure in keymap.h
+    // rgb_time_out_enable = false;                        // Disable RGB timeout by default. Enable using toggle key.
+    // rgb_time_out_user_value = false;                    // Has to have the same initial value as rgb_time_out_enable.
+    // rgb_enabled_flag = true;                            // Initially, keyboard RGB is enabled. Change to false config.h initializes RGB disabled.
+    // rgb_time_out_fast_mode_enabled = false;             // RGB timeout fast mode disabled initially.
+    // rgb_time_out_saved_flag = rgb_matrix_get_flags();   // Save RGB matrix state for when keyboard comes back from ide.
+};
 
 // void keyboard_post_init_user(void) {
 //     rgb_matrix_enable();
@@ -300,7 +265,7 @@ void matrix_scan_user(void) {
     if (last_animation_change == 0)
         last_animation_change = timer_read();
 
-    if (timer_elapsed(last_animation_change) >  5 * 1000) {
+    if (timer_elapsed(last_animation_change) >  5 * MILLISECONDS_IN_SECOND) {
         last_animation_change = timer_read();
         rgb_matrix_step();
     }
